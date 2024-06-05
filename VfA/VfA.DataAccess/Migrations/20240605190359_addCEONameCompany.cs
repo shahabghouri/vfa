f26c7d7097837aacd782efb49c742525ac2a258a
Saad Javed <saad.javed@iengineering.com>
2024-06-06 00:05:10 +0500

    adding CEOName migration

---
+++ VfA/VfA.DataAccess/Migrations/20240605190359_addCEONameCompany.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VfA.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCEONameCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CEOName",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CEOName",
+                table: "Companies");
+        }
+    }
+}
