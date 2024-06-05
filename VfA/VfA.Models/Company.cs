b89ac5d744506010b8c24c88d963b6c1e331bcac
Saad Javed <saad.javed@iengineering.com>
2024-06-06 00:05:01 +0500

    adding CEOName

--- VfA/VfA.Models/Company.cs
+++ VfA/VfA.Models/Company.cs
@@ -12,6 +12,7 @@ namespace VfA.Models {
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        public string? CEOName { get; set; }
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
