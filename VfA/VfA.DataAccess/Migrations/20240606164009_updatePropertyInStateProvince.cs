1045b1a9a07dec83cd286aeffdf3a87d38e42ecc
Saad Javed <saad.javed@iengineering.com>
2024-06-07 00:41:00 +0500

    migration related to entity changes

---
+++ VfA/VfA.DataAccess/Migrations/20240606164009_updatePropertyInStateProvince.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VfA.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePropertyInStateProvince : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CountryId",
+                table: "StateProvince",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "StateProvince");
+        }
+    }
+}
