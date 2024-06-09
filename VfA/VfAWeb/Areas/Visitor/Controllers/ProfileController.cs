0d502b0b78120a2f9ff82d8f9b6adff9614658fb
Saad Javed <saad.javed@iengineering.com>
2024-06-09 20:01:58 +0500

    adding profile page and content

---
+++ VfA/VfAWeb/Areas/Visitor/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VfAWeb.Areas.Visitor.Controllers
+{
+    [Area("Visitor")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VfA/VfAWeb/wwwroot/js/ProductDisplayCarousel/ProductCarousel.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+	$(".wish-icon i").click(function () {
+		$(this).toggleClass("fa-heart fa-heart-o");
+	});
+});	
